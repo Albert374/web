0ac4e1b78beb128568abfa6420ae1e707ab31a53
Albert374 <likenice122@gmail.com>
2024-10-12 02:54:56 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.getElementById('miFormulario').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    
+
+    var nombre = this.nombre.value;
+    var telefono = this.telefono.value;
+
+
+    console.log('Nombre:', nombre);
+    console.log('Teléfono:', telefono);
+    
+    alert('Datos capturados. Revisa la consola para verlos.');
+    
+    document.getElementById('registro').style.display = 'none';
+    document.getElementById('contenido').style.display = 'block';
+});

d1b2fdf54105026eaf8b12fb44519c8b303b2394
Albert374 <likenice122@gmail.com>
2024-10-17 08:01:46 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,26 @@
 document.getElementById('miFormulario').addEventListener('submit', function(event) {
     event.preventDefault(); 
 
-    var nombre = this.nombre.value;
-    var telefono = this.telefono.value;
+    let nombre = this.nombre.value;
+    let edad = this.edad.value;
+    let email = this.email.value;
+
 
     console.log('Nombre:', nombre);
-    console.log('Teléfono:', telefono);
+    console.log('edad:', edad);
+    console.log('email:', email);
+
     
     alert('Datos capturados. Revisa la consola para verlos.');
     
     document.getElementById('registro').style.display = 'none';
     document.getElementById('contenido').style.display = 'block';
 });
+
+function openSidebar() {
+    document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeSidebar() {
+    document.getElementById("mySidebar").style.width = "0";
+}
